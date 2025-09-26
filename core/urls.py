22f31648aa76a1e22ef285692ac61c68816f6568
abdul azeez <azeezupup@gmail.com>
2025-09-26 17:39:17 +0530

    Fix contact URL reverse issue and update template links

--- core/urls.py
+++ core/urls.py
@@ -9,7 +9,6 @@ urlpatterns = [
 
     # Public pages for SEO & AdSense
     path('about/', views.about_view, name='about'),
-    # path('contact/', views.contact_view, name='contact'),
     path('privacy-policy/', views.privacy_policy_view, name='privacy_policy'),
     path('terms-conditions/', views.terms_conditions_view, name='terms_conditions'),
     path('faq/', views.faq_view, name='faq'),
