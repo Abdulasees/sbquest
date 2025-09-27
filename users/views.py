b57575b3c6f65ae2015969099def96e8b0cddc59
abdul azeez <azeezupup@gmail.com>
2025-09-27 09:29:58 +0530

    Updated templates, added delete account functionality, updated views

--- users/views.py
+++ users/views.py
@@ -4,6 +4,7 @@ from django.contrib.auth.forms import AuthenticationForm
 from django.contrib import messages
 from .forms import CustomUserCreationForm
 from django.contrib.auth.decorators import login_required
+import os
 
 
 def signup_view(request):
@@ -39,5 +40,28 @@ def logout_view(request):
 @login_required
 def profile_view(request):
     return render(request, 'profile.html')
+
+
+@login_required
+def delete_account_view(request):
+    if request.method == 'POST':
+        user = request.user
+
+        # Delete related files if user has a profile with avatar
+        if hasattr(user, 'profile'):
+            if user.profile.avatar and os.path.isfile(user.profile.avatar.path):
+                os.remove(user.profile.avatar.path)
+
+        # Log out the user
+        logout(request)
+
+        # Delete the user account
+        # This will also delete all related models with on_delete=CASCADE
+        user.delete()
+
+        messages.success(request, "Your account and all personal data, including wallet transactions, have been deleted successfully.")
+        return redirect('users:signup')  # Redirect to signup/login page
+
+    return render(request, 'delete_account.html')
 
 
